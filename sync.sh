44efda103b1f6ded55050b276be1678c4da3f9c0
rechi2 <146033060+rechi2@users.noreply.github.com>
2025-02-06 08:30:01 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -15,7 +15,7 @@ if test "$(rclone ls $SYNC_SRC $RCLONE_OPTS)"; then
   # the source directory is not empty
   # it can be synced without clear data loss
   echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
-  rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+  rclone bisync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
 
   if [ -z "$CHECK_URL" ]
   then
